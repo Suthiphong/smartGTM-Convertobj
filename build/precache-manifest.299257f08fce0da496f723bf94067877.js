0e8e771f66671f2adaa8a32e35c5af32bfa9d759
Sutthipong Thaisuriya <cg610076@ptt.corp>
2019-05-27 09:10:55 +0700

    Build

---
+++ build/precache-manifest.299257f08fce0da496f723bf94067877.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ab50136233ef16b91b91bb04993c69",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce28f1396ad196112c15",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7c6cb50eec25fbd9759d",
+    "url": "/static/css/main.2be691da.chunk.css"
+  },
+  {
+    "revision": "ce28f1396ad196112c15",
+    "url": "/static/js/2.daec0516.chunk.js"
+  },
+  {
+    "revision": "7c6cb50eec25fbd9759d",
+    "url": "/static/js/main.890ca486.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
